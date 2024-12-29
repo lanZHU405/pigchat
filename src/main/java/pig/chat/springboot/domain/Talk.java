7f39a615f5a38da0a0fab96f178212ab5ee8b2e6
linjunyi <743228364@qq.com>
2024-12-29 15:52:11 +0800

    添加mongodb

--- src/main/java/pig/chat/springboot/domain/Talk.java
+++ src/main/java/pig/chat/springboot/domain/Talk.java
@@ -1,11 +1,32 @@
 package pig.chat.springboot.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 @TableName("talk")
 public class Talk {
 
     private String id;
+
+    // 发送者id
+    @TableField("sender_id")
+    private String senderId;
+
+    // 接收者id
+    @TableField("received_id")
+    private String receiveId;
+
+    // 消息内容
+    @TableField("message")
+    private String message;
+
+    @TableField("send_at")
+    private Date sendAt;
+
+    @TableField("is_read")
+    private boolean isRead;
 }
