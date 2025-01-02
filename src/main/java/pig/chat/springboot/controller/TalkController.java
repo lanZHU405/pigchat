9e7100b5f8467e34076d04350281dc0e2e1e0cc7
linjunyi <743228364@qq.com>
2025-01-02 16:38:16 +0800

    添加mongodb

---
+++ src/main/java/pig/chat/springboot/controller/TalkController.java
@@ -0,0 +1,12 @@
+package pig.chat.springboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/talk")
+@Slf4j
+public class TalkController {
+
+}
